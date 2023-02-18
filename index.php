3fc91b5e6a397c57de7325487b7aee1a76eafad9
TThanhPhong <ttp15112001@gmail.com>
2023-02-18 18:45:43 +0700

    commit 18/02/2023

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Shop Bán Giầy</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" >
+</head>
+<body>
+	<div class="wrapper">
+		<!-- header -->
+		<div class="header">
+			<div class="row">
+					<div class="col">
+						<a href="#"><img src="images/logojordan.png" alt="" class="logojordan"></a>
+					</div>
+					<div class="col">
+						<ul class="list_1">
+							<li><a href="#">Đăng Nhập</a></li>
+							<li><a href="#">Địa Chỉ<span class="null">|</span></a></li>
+							<li><a href="#">Trợ Giúp<span class="null">|</span></a></li>
+							<li><a href="#">Chúng Tôi<span class="null">|</span></a></li>
+						</ul>
+					</div>
+				</div>
+		</div>
+		<!-- menu -->
+		<div class="menu">
+			<div class="row">
+				<div class="col">
+					<a href="#"><img src="images/logo.png" alt="" class="logo"></a>
+				</div>
+				<div class="col">
+					<ul>
+						<li><a href="#">Trang Chủ</a></li>
+						<li><a href="#">Đàn Ông</a></li>
+						<li><a href="#">Phụ Nữ</a></li>
+						<li><a href="#">Trẻ Em</a></li>
+						<li><a href="#">Sale</a></li>
+					</ul>
+				</div>
+				<div class="col">
+					<ul>
+						<li><a href="#">Trang Chủ</a></li>
+						<li><a href="#">Đàn Ông</a></li>
+						<li><a href="#">Phụ Nữ</a></li>
+						<li><a href="#">Trẻ Em</a></li>
+						<li><a href="#">Sale</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- banner -->
+		<img src="images/banner1.gif" alt="" class="banner">
+
+		<!-- main -->
+		<div class="main">
+			<p>main</p>
+		</div>
+		<!-- footer -->
+		<div class="footer">
+			<p>footer</p>
+		</div>
+	</div>
+</body>
+</html>
